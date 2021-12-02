96440dba3e2694eda80de40866bfb3cf071cd68a
Ahmy Yulrizka <yulrizka@users.noreply.github.com>
2021-12-02 20:24:06 +0100

    python: 2021-2 with match python 3.10

--- 2021/python/day2/day1.py
+++ 2021/python/day2/day1.py
@@ -7,14 +7,13 @@ data = [line.strip('\n') for line in fileinput.input("../../input/day2")]
 def part1():
     p = (0, 0)
     for line in data:
-        dir, count = line.split(' ')
-        count = int(count)
-        if dir == 'up':
-            p = (p[0], p[1] - count)
-        elif dir == 'down':
-            p = (p[0], p[1] + count)
-        elif dir == 'forward':
-            p = (p[0] + count, p[1])
+        match line.split(' '):
+            case 'up', n:
+                p = (p[0], p[1] - int(n))
+            case 'down', n:
+                p = (p[0], p[1] + int(n))
+            case 'forward', n:
+                p = (p[0] + int(n), p[1])
     return p[0] * p[1]
 
 
@@ -22,16 +21,14 @@ def part2():
     p = (0, 0)
     aim = 0
     for line in data:
-        dir, count = line.split(' ')
-        count = int(count)
-        if dir == 'up':
-            aim -= count
-        elif dir == 'down':
-            aim += count
-        elif dir == 'forward':
-            p = (p[0] + count, p[1] + (aim * count))
+        match line.split(' '):
+            case 'up', n:
+                aim -= int(n)
+            case 'down', n:
+                aim += int(n)
+            case 'forward', n:
+                p = (p[0] + int(n), p[1] + (aim * int(n)))
     return p[0] * p[1]
-
 
 
 class TestSum(unittest.TestCase):
@@ -40,4 +37,4 @@ class TestSum(unittest.TestCase):
         assert part1() == 1936494
 
     def test2(self):
-        assert part2() == 1936494
+        assert part2() == 1997106066
