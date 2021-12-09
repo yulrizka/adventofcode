963cb347d8ace813ec877f4a988b180ba3e9428b
Ahmy Yulrizka <yulrizka@users.noreply.github.com>
2021-12-09 08:08:00 +0100

    add script to generate skeleton

---
+++ new.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 3 ]; then
+  echo "use $0 [python] [year] [day]"
+  exit 1
+fi
+
+year=$2
+day=$3
+
+function getInput() {
+  aocd $day $year > "$year/input/day$day"
+}
+
+function python() {
+  getInput
+  mkdir -p "$year/python/day$day"
+  cat <<EOF > "$year/python/day$day/day$day.py"
+import fileinput
+import unittest
+
+data = [int(i) for i in fileinput.input("../../input/day$day").readline().split(',')]
+print(data)
+
+
+def part1():
+    ...
+
+
+def part2():
+    ...
+
+
+class TestSum(unittest.TestCase):
+
+    def test1(self):
+        ans = part1()
+        print(ans)
+        assert ans == 0
+
+    def test2(self):
+        ans = part2()
+        print(ans)
+        assert ans == 0
+EOF
+
+  echo "python"
+}
+
+case $1 in
+  "python")
+    python
+    ;;
+
+  *)
+    echo "$1 not known"
+    exit 1
+    ;;
+
+esac
+
